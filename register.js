8d35fbb0db86be0a1c51fa85e0bfe2b88f2a859c
Modestas Apanavicius <modestas.apanavicius@gmail.com>
2023-01-16 22:14:30 +0200

    tryToPost

--- register.js
+++ register.js
@@ -1,8 +1,24 @@
-function inputFill(inputData) {
-  var btn = document.getElementById("registerBtn");
-  if (inputData.value.length > 0) {
-    btn.disabled = false;
-  } else {
-    btn.disabled = true;
-  }
-}
+document.getElementById("registerForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const password = document.getElementById("password").value;
+  const username = document.getElementById("username").value;
+  const users = {
+    username: username,
+    password: password,
+  };
+  fetch("https://localhost:7068/User", {
+    method: "POST",
+    body: JSON.stringify(users),
+    headers: {
+      Accept: "text/plain",
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    if (res.ok) {
+      alert("Užklausa sėkminga");
+      window.location.replace("index.html");
+    } else {
+      alert("Užklausa nesėkminga");
+    }
+  });
+});
