e8506938cb9fd32c96ad65a42d516540aa6ac1ca
Modestas Apanavicius <modestas.apanavicius@gmail.com>
2023-01-18 22:01:48 +0200

    picture

--- uploadinfo.js
+++ uploadinfo.js
@@ -5,27 +5,37 @@ document.getElementById("detailsForm").addEventListener("submit", (e) => {
   const personCode = document.getElementById("personCode").value;
   const telNumber = document.getElementById("telNumber").value;
   const email = document.getElementById("email").value;
-  const img = document.getElementById("img").value;
+  const city = document.getElementById("city").value;
+  const street = document.getElementById("street").value;
+  const houseNumber = document.getElementById("houseNumber").value;
+  const flatNumber = document.getElementById("flatNumber").value;
+  //const picture = document.getElementById("img").value;
+
+  let base64String = "";
   function imageUploaded() {
     var file = document.getElementById("img")["files"][0];
     var reader = new FileReader();
-    let base64String = "";
     reader.onload = function () {
       base64String = reader.result.replace("data:", "").replace(/^.+,/, "");
+      imageBase64Stringsep = base64String;
       console.log(base64String);
     };
     reader.readAsDataURL(file);
     return base64String;
   }
+  var pic = imageUploaded();
+  console.log(pic);
   const user = {
     name: name,
     lastName: lastName,
     personCode: personCode,
     telNumber: telNumber,
     email: email,
-    img: "123",
-    addressid: 1,
-    userid: 17,
+    picture: picture,
+    city: city,
+    street: street,
+    houseNumber: houseNumber,
+    flatNumber: flatNumber,
   };
   fetch("https://localhost:7068/Person", {
     method: "POST",
