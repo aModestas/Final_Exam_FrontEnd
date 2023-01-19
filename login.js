f0d9cacc2f3c2b8006dded75d8d0f05c0f7f7cea
Modestas Apanavicius <modestas.apanavicius@gmail.com>
2023-01-19 21:08:47 +0200

    login works

---
+++ login.js
@@ -0,0 +1,25 @@
+document.getElementById("loginForm").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const password = document.getElementById("password").value;
+  const username = document.getElementById("username").value;
+  const user = {
+    username: username,
+    password: password,
+    role: "string",
+  };
+  fetch("https://localhost:7068/User").then((response) => {
+    response.json().then((data) => {
+      let login = data.filter(
+        (element) =>
+          element.username == username && element.password == password
+      );
+      if (login.length == 1) {
+        console.log("Success");
+        window.location.replace("afterLogin.html");
+      } else {
+        console.log("Wrong details");
+        alert("Wrong username or password");
+      }
+    });
+  });
+});
